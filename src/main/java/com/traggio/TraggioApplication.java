189a7da0c32461e372bc08d56567ecc75cef7a30
Hilixx <jhonalizan@gmail.com>
2025-01-28 08:03:21 -0300

    teste

---
+++ src/main/java/com/traggio/TraggioApplication.java
@@ -0,0 +1,13 @@
+package com.traggio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraggioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraggioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/traggio/TraggioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traggio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraggioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
